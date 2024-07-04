551918d1aefd507938100359f1664a00a03d6c29
Ali Hamdi Ali Fadel <aliosm1997@gmail.com>
2024-07-04 07:40:15 +0000

    Update DevContainer settings

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,6 +7,4 @@ gem update --system
 bundle
 yarn
 
-sudo sed -i 's/rake routes/rails routes/g' /usr/local/rvm/gems/ruby-3.2.2/gems/annotate-3.2.0/lib/annotate/annotate_routes/header_generator.rb
-
 rails db:create db:migrate db:seed
